c47db25721d306086eb0e5f24bf6371cef275b37
koleso8 <dev.koleso@gmail.com>
2024-11-23 16:21:04 +0200

    add menu

---
+++ src/js/menu.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('.menu-overlay');
+const menuButton = document.querySelector('.header-menu-button');
+const closeMenuButton = document.querySelector('.close-modal');
+// document.addEventListener('DOMContentLoaded', () => {});
+
+const handleOpenModal = () => {
+  menu.classList.toggle('isOpen');
+};
+menuButton.addEventListener('click', handleOpenModal);
+closeMenuButton.addEventListener('click', handleOpenModal);
